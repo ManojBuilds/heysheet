d07ae98156310c75b909cb11295f44fd664e2540
Manoj Kumar <ms8460149@gmail.com>
2025-06-21 08:59:42 +0530

    feat: add logo image component and update logo display in the header

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -1,12 +1,20 @@
 import { ZapIcon } from "lucide-react";
 import Link from "next/link";
+import Image from "next/image";
 
 export const Logo = () => {
   return (
     <Link href={"/"} className="inline-flex items-center gap-1">
       {/* <ZapIcon className="text-primary"/> */}
       {/* 🦍 */}
-      <span className="font-semibold text-2xl font-logo">heysheet</span>
+      {/* <span className="font-semibold text-2xl font-logo">heysheet</span> */}
+      <Image
+      src={'/logo.png'}
+      alt="Heysheet Logo"
+      width={177}
+      height={40}
+      className="object-contain h-10"
+      />
     </Link>
   );
 };
