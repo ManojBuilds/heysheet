baab232f7a69d31ac0cf93a6a18f6cce7660b4bc
Manoj Kumar <ms8460149@gmail.com>
2025-06-26 07:50:49 +0530

    remove cookie consent

--- src/components/Provider.tsx
+++ src/components/Provider.tsx
@@ -3,8 +3,6 @@
 import { ClerkProvider } from "@clerk/nextjs";
 import { ReactNode } from "react";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-
-import CookieConsent from "react-cookie-consent";
 import { PostHogProvider } from "./PostHogProvider";
 import { dark } from "@clerk/themes";
 
@@ -25,32 +23,6 @@ export default function Provider({ children }: { children: ReactNode }) {
       }
 
     }} >
-      <CookieConsent
-        location="bottom"
-        buttonText="Accept"
-        cookieName="heysheetCookieConsent"
-        style={{
-          background: "#0f0f0f",
-          color: "#eaeaea",
-          fontFamily: "DM Sans",
-          fontSize: "14px",
-          padding: "1rem 2rem",
-          boxShadow: "0 -2px 10px rgba(0,0,0,0.3)",
-        }}
-        buttonStyle={{
-          background: "#facc15",
-          color: "#0f0f0f",
-          fontWeight: "600",
-          fontSize: "14px",
-          borderRadius: "6px",
-          padding: "10px 16px",
-        }}
-        expires={150}
-      >
-        We use cookies to improve your experience on Heysheet. By continuing,
-        you agree to our cookie policy.
-      </CookieConsent>
-
       <QueryClientProvider client={queryClient}  >
         <PostHogProvider>{children}</PostHogProvider>
       </QueryClientProvider>
