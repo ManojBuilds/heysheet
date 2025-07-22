e12931ac8f56b1cfed2981cf0e9b88f864f37944
Manoj Kumar <ms8460149@gmail.com>
2025-07-22 09:41:40 +0530

    remove not found

--- src/app/(main)/dashboard/page.tsx
+++ src/app/(main)/dashboard/page.tsx
@@ -24,7 +24,6 @@ export default async function DashboardPage({
 
   const fromDateIso = fromDate.toISOString();
   const toDateIso = toDate.toISOString();
-  return notFound()
 
   return (
     <div className="flex flex-col gap-4">
