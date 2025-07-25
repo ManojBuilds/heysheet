ecc5a97905509d663c22e0e1c211b4ab6594d662
Manoj Kumar <ms8460149@gmail.com>
2025-07-25 19:37:52 +0530

    fix clerk loading

--- src/components/MyUserButton.tsx
+++ src/components/MyUserButton.tsx
@@ -1,25 +1,28 @@
 'use client'
 import { config } from "@/config"
-import { UserButton } from "@clerk/nextjs"
+import { UserButton, useUser } from "@clerk/nextjs"
 import { dark, neobrutalism } from "@clerk/themes"
 import { useTheme } from "next-themes"
 
 export const MyUserButton = () => {
     const { resolvedTheme } = useTheme()
+    const { isLoaded } = useUser();
+
     return (
-        <UserButton
-            appearance={{
-                baseTheme: resolvedTheme === "dark" ? dark : neobrutalism,
-                layout: {
-                    logoLinkUrl: "/",
-                    logoPlacement: "inside",
-                    termsPageUrl: config.landingPageUrl + "/terms",
-                    privacyPageUrl: config.landingPageUrl + "/privacy-policy",
-                    shimmer: true,
-                    socialButtonsVariant: "blockButton",
-                    socialButtonsPlacement: "top",
-                },
-            }}
-        />
+        isLoaded ?
+            <UserButton
+                appearance={{
+                    baseTheme: resolvedTheme === "dark" ? dark : neobrutalism,
+                    layout: {
+                        logoLinkUrl: "/",
+                        logoPlacement: "inside",
+                        termsPageUrl: config.landingPageUrl + "/terms",
+                        privacyPageUrl: config.landingPageUrl + "/privacy-policy",
+                        shimmer: true,
+                        socialButtonsVariant: "blockButton",
+                        socialButtonsPlacement: "top",
+                    },
+                }}
+            /> : null
     )
 }
